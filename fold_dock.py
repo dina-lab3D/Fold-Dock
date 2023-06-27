dbf2513ae88a23df93f96ff9b27a4514cc3c29f4
tomer.cohen13 <tomer.cohen13@mail.huji.ac.il>
2023-06-27 23:25:12 +0300

    fixed bugs

--- fold_dock.py
+++ fold_dock.py
@@ -6,7 +6,7 @@ import pandas as pd
 import logging
 from timeit import default_timer as timer
 from utils import *
-from network_input import get_antigen_input, get_antibody_input, convert_dock_input_to_score_input
+from networks_input import get_antigen_input, get_antibody_input, convert_dock_input_to_score_input
 from matrix_to_pdb import matrix_to_pdb_antibody, matrix_to_pdb_antigen
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
@@ -56,14 +56,17 @@ def dock_and_fold(ab_sequence, ag_model, ag_seq, ag_input, dock, ab_score,
     """
     runs Fold&Dock structure predictions
     """
+    print("Making input for the folding and docking network")
     ab_input = get_antibody_input(ab_sequence.seq)
     ab_input = np.array([np.array(ab_input) for _ in range(len(ag_input))])
 
     # Fold, Dock and Score
+    print("Predicting the antibody-antigen complexes" if ag_model is not None else "Predicting the antibody structure")
     fold_dock_results = dock.predict([ab_input, ag_input])
     ab_score_input, ag_score_input, pred_ab, pred_ag = convert_dock_input_to_score_input(ab_input, ag_input, fold_dock_results)
 
     if ag_model is not None:
+        print("Calculating scores for the antibody-antigen complexes")
         scores = nb_score.predict([ab_score_input, ag_score_input]) if is_nb(ab_sequence.seq) else ab_score.predict([ab_score_input, ag_score_input])
         scores = -1 * np.array(scores).flatten()
     else:
@@ -71,13 +74,14 @@ def dock_and_fold(ab_sequence, ag_model, ag_seq, ag_input, dock, ab_score,
     # get models by rank
     ranks = np.argsort(scores)
     pd.DataFrame({"model": ["{}_rank_{}".format(ab_sequence.id, i+1) for i in range(len(ranks))], "score": scores[ranks]}).to_csv("scores.csv")
-
-    print("")
+    print("Finished running Fold&Dock{}".format("" if ag_model is None else ", best comlex model score: " + str(scores[0])))
 
     # output models for top n results
-    for rank, pred_model in enumerate(ranks[:topn]):
-        model_file_path = "{}_rank_{}_unrelaxed.pdb".format(ab_sequence.id, rank + 1)
-        make_pdb_files(model_file_path, ab_sequence.seq, pred_ab[pred_model], ag_seq=ag_seq, ag_model=ag_model, ag_pred=pred_ag[pred_model], run_modeller=run_modeller)
+    if topn > 0:
+        print("Creating PDB files for the top {} models".format(min(topn, len(ranks))))
+        for rank, pred_model in enumerate(ranks[:topn]):
+            model_file_path = "{}_rank_{}_unrelaxed.pdb".format(ab_sequence.id, rank + 1)
+            make_pdb_files(model_file_path, ab_sequence.seq, pred_ab[pred_model], ag_seq=ag_seq, ag_model=ag_model, ag_pred=pred_ag[pred_model], run_modeller=run_modeller)
 
 
 def dock_and_fold_batch(ab_fasta, ag_pdb, antigen_chains, dock, ab_score,
@@ -123,10 +127,10 @@ def check_input_args():
     if not os.path.exists(args.ab_fasta):
         print("Can't find the given antibody fasta file: '{}', aborting.".format(args.ab_fasta), file=sys.stderr)
         exit(1)
-    if not os.path.exists(args.ag_pdb):
+    if args.ag_pdb and not os.path.exists(args.ag_pdb):
         print("Can't find the given antigen pdb file: '{}', aborting.".format(args.ag_pdb), file=sys.stderr)
         exit(1)
-    if not args.antigen_chains.isalpha():
+    if args.ag_pdb and not args.antigen_chains.isalpha():
         print("Antigen pdb chains should contain only a-z,A-Z characters: '{}', aborting.".format(args.antigen_chains), file=sys.stderr)
         exit(1)
     if not os.path.exists(dock_model) or not os.path.exists(ab_score_model) or not os.path.exists(nb_score_model):
@@ -172,7 +176,7 @@ if __name__ == '__main__':
         from relax_pdb import relax_pdb
 
     start = timer()
-    dock_and_fold_batch(args.ab_fasta, os.path.abspath(args.ag_pdb), args.antigen_chains, dock_model, ab_score_model,
+    dock_and_fold_batch(args.ab_fasta, input_ag_pdb, args.antigen_chains, dock_model, ab_score_model,
                         nb_score_model, args.topn, args.modeller, output_directory)
     end = timer()
 
